cf6f091c78f0c4ef5d17a92e94397dd8f6e2bc32
ElangeP <elange.pierre@gmail.com>
2023-01-18 03:02:00 -0500

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -23,8 +23,8 @@ resource "aws_instance" "nginx1" {
 #! /bin/bash
 sudo amazon-linux-extras install -y nginx1
 sudo service nginx start
-aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/website/index.html /home/ec2-user/index.html
-aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png /home/ec2-user/Globo_logo_Vert.png
+aws s3 cp s3://${aws_s3_bucket.aniscleaning-web_bucket.id}/website/index.html /home/ec2-user/index.html
+aws s3 cp s3://${aws_s3_bucket.aniscleaning-web_bucket.id}/website/Globo_logo_Vert.png /home/ec2-user/Globo_logo_Vert.png
 sudo rm /usr/share/nginx/html/index.html
 sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
 sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png
@@ -44,8 +44,8 @@ resource "aws_instance" "nginx2" {
 #! /bin/bash
 sudo amazon-linux-extras install -y nginx1
 sudo service nginx start
-aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/website/index.html /home/ec2-user/index.html
-aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png /home/ec2-user/Globo_logo_Vert.png
+aws s3 cp s3://${aws_s3_bucket.aniscleaning-web_bucket.id}/website/index.html /home/ec2-user/index.html
+aws s3 cp s3://${aws_s3_bucket.aniscleaning-web_bucket.id}/website/Globo_logo_Vert.png /home/ec2-user/Globo_logo_Vert.png
 sudo rm /usr/share/nginx/html/index.html
 sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
 sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png
